c8840b1b2f2a145e6f19c55174db5ff4e457a1dd
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-10 14:55:45 +0200

    errorHandler added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import colors from 'colors';
 
 import { connectDB } from "./config/db.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -11,5 +12,10 @@ connectDB();
 const app = express();
 
 const port = process.env.PORT
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,10 @@
+export const errorHandler = (err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message || 'Something went Wrong!'
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+    })
+}
