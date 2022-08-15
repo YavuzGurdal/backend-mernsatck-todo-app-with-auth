4f818985b9b736068067e28c7fbd479714879840
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-15 19:55:44 +0200

    last version

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conndb = await mongoose.connect(process.env.MONGO_URI);
+        const conndb = mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB connected: ${conndb.connection.host}`.cyan.underline);
     } catch (err) {
