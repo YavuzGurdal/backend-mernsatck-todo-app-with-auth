6eb4a77458b295935d7ae1099aaaae812ee911a3
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-15 22:18:22 +0200

    some changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conndb = mongoose.connect(process.env.MONGO_URI);
+        const conndb = mongoose.connect(process.env.MONGO_URI, { // burdan db ye baglaniyorum
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            //useFindAndModify: false,
+        });
 
         console.log(`MongoDB connected: ${conndb.connection.host}`.cyan.underline);
     } catch (err) {
