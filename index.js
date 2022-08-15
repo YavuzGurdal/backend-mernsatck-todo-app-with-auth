68a517ef969f1089235d9f015cffd9911358cf57
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-15 18:13:56 +0200

    my first commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 
 const app = express();
 
-const port = process.env.PORT
+// const port = process.env.PORT
 
 app.use(cookieParser())
 app.use(express.json())
@@ -23,6 +23,13 @@ app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use('/api/todos', todoRoutes) // todoRoutes icindeki her route'a burdan gidiyor
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
+
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+// app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(process.env.PORT || 8080, () => console.log(`Server started on port ${port}`))
